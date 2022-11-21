55344a58d05dc6d1fdd02256b3cfed1a132ed451
Dmirty Kiselev <kiselevzfth@gmail.com>
2022-11-22 01:18:03 +0400

    настроил подключение к бд

--- back/bjd_tests/bjd_tests/settings.py
+++ back/bjd_tests/bjd_tests/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bjd_tests.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bjd',
+        'USER': 'postgres',
+        'PASSWORD': '00023',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
